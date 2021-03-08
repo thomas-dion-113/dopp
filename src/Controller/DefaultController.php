772813429583b729f4c546645a4cbe8d62a55a60
Thomas Dion <thomasdion@hotmail.com>
2021-03-08 13:19:01 +0100

    debug ios

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,11 @@ class DefaultController extends AbstractController
     /**
      * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
      */
-    public function index()
+    public function index(Request $request)
     {
+        if (str_contains($request->getUri(), '.css')) {
+            return new Response(null, 404);
+        }
         return $this->render('default/index.html.twig');
     }
 }
