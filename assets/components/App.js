09fca63810bf95a037960e49baf02547594f83b5
Thomas Dion <thomasdion@hotmail.com>
2021-03-08 21:43:32 +0100

    ios debug

--- assets/components/App.js
+++ assets/components/App.js
@@ -100,7 +100,7 @@ class App extends Component {
                 console.log("HIDE NAVBAR 0");
                 setTimeout(() => {
                     console.log("HIDE NAVBAR 1");
-                    $('.navbar-collapse').collapse('hide');
+                    // $('.navbar-collapse').collapse('hide');
                     console.log("HIDE NAVBAR 2");
                 }, 150);
             });
